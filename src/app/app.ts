915e5f3bf4bc2aa4e2532363966332ac23b0ee0a
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2025-08-28 16:19:47 +0300

    Data binding features added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, Home, FormsModule],
+  //templateUrl: './app.html',
+  template:`
+    <h1 style="color: aqua;">ANGULAR</h1>
+    <div>Hello {{title}}!</div>
+    <p>Meslek: {{name}}</p>
+    <img [src] = "src" [width] = "width">
+    <br>
+    <input type="text" value = "-" >
+    <br >
+    <button (click) = "submitButton()">SUBMIT</button>
+    <br>
+    <input type="text" [(ngModel)] = "twoWayInput" (ngModelChange) = "onChange($event)"> <!-- FormsModule import edilmesi unutulmamalıdır.-->
+    <br>
+    {{twoWayInput}}
+    <app-home [pageName] = "pageName"></app-home>
+  `,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = 'Seyyit Battal';
+  title: string = "Seyyit Battal ARVAS";
+  name: string = 'Software Engineer';
+  src: string = "https://yt3.googleusercontent.com/L7fPOHO5TwVC_qz78Yo8P0MuJAB8I4FdQmOY2HSW7Z4_AFJZgAHzJCKQXo7ek1sy0pj0dIzM2Xg=s900-c-k-c0x00ffffff-no-rj";
+  width: number = 100;
+  pageName: string = "Home Sayfası";
+  submitButton(){
+    console.log("Submit butonuna tıklandı.");
+  }
+  twoWayInput: string = "";
+  onChange(value: string){
+    this.twoWayInput = value;
+    console.log(value);
+  }
+
 }
+   
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
-  templateUrl: './home.html',
+  //templateUrl: './home.html',
+  template: `
+  <h2>Bu sayfanın adı: {{pageName}}</h2>
+  ` ,
   styleUrl: './home.css'
 })
 export class Home {
+  @Input() pageName: string = ""; //App'ten geliyor ancak ilk değer vermeden geçilemiyor. Çözüm: tsconfig dosyasında "strict": false yapmak.
 
 }
+
+
+
